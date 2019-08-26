f2b41918b8a6851ec6af7dedf35425189278c680
Chamle <chamle1602@gmail.com>
2019-08-26 23:03:43 +0900

    Create LoginView.java

---
+++ LoginManagement/src/View/LoginView.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import Entity.User;
+import java.util.Scanner;
+
+/**
+ *
+ * @author chaml
+ */
+public class LoginView {
+    User user;
+
+    public LoginView(User user) {
+        this.user = user;
+    }
+    public void showMessage(String smg){
+        System.out.println(smg);
+    }
+    public void getUserInfo(){
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Username: ");
+            user.setName(input.nextLine());
+            System.out.print("Password: ");
+            user.setPassword(input.nextLine());
+//            wait();
+            input.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
